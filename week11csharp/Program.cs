13d5b185c1f3e9130d5fa5b13e49978ff5224ac9
JISNA <JISNA@LAPTOP-METFMLOC>
2024-03-31 17:52:50 -0400

    Add project files.

---
+++ week11csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using week11csharp.EF.Models.Entities;
+
+namespace week11csharp.EF
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var dbContext = new MMABooksContext())
+            {
+                var customers = dbContext.Customers;
+
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine(customer.Name);
+                }
+            }
+        }
+    }
+}
